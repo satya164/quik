54a74d43ddf7297a0c5a5f39be10001983c28962
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-12-29 00:33:52 +0530

    Remove unused file

--- src/setup-template.js
+++ src/setup-template.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const ncp = require('ncp');
-const setupDeps = require('./setup-deps');
 
 const BABEL_PRESETS = [ 'react', 'es2015', 'stage-1' ];
 
@@ -13,8 +12,6 @@ module.exports = function(name) {
         if (err) {
             return console.error(err);
         }
-
-        // setupDeps(projectDir, BABEL_PRESETS.map(preset => 'babel-preset-' + preset));
 
         console.log('Project initialized successfully!');
     });
