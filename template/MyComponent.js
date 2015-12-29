04f0f0e44801f2ff3ae659c8a8bdce942f402701
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-12-30 03:01:16 +0530

    Update templates

--- template/MyComponent.js
+++ template/MyComponent.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
+
+const styles = {
+    container: {
+        margin: 16,
+        fontFamily: 'sans-serif',
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#443b5d'
+    }
+};
 
 class MyComponent extends Component {
-  render() {
-    return <div>Hello world!</div>
-  }
+    render() {
+        return <div style={styles.container}>Hello world!</div>;
+    }
 }
 
-export default MyComponent;
+export default Radium(MyComponent);
