44b6b6c687e971d53b85a01a9546cf4cab7f226a
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-01-01 09:08:38 +0530

    Remove .eslintrc from starter project

--- template/index.js
+++ template/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MyComponent from './MyComponent';
 
-ReactDOM.render(<MyComponent />, document.getElementById('root'))
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
