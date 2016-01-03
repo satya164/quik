263de4522bf2b9c9176c9a56e7e3aae8f9617f05
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-01-04 00:29:40 +0530

    Add test for --init

--- __tests__/cli-test.js
+++ __tests__/cli-test.js
@@ -3,6 +3,14 @@
 const test = require('blue-tape');
 const path = require('path');
 const child_process = require('child_process');
+const fs = require('fs');
+const del = require('del');
+const mkdirp = require('mkdirp');
+
+const TESTDIR = '/tmp/quik-test-' + Date.now();
+const PROJECT_NAME = 'AwesomeProject';
+
+test('setup', () => del(TESTDIR, { force: true }));
 
 test('should print usage', t => {
     child_process.execFile(path.join(__dirname, '../bin/quik.js'), [ '--help' ], {}, (err, stdout) => {
@@ -14,3 +22,32 @@ test('should print usage', t => {
         }
     });
 });
+
+test('should initialize project with template', t => {
+    mkdirp(TESTDIR, e => {
+        if (e) {
+            t.fail(e.message);
+        } else {
+            child_process.execFile(path.join(__dirname, '../bin/quik.js'), [ '--init', PROJECT_NAME ], {
+                cwd: TESTDIR
+            }, err => {
+                if (err) {
+                    t.fail(err);
+                } else {
+                    fs.readdir(path.join(TESTDIR, PROJECT_NAME), (error, res) => {
+                        if (error) {
+                            t.fail(error.message);
+                        } else {
+                            t.ok(res.indexOf('index.html') > -1, 'index.html');
+                            t.ok(res.indexOf('index.js') > -1, 'index.js');
+                            t.ok(res.indexOf('MyComponent.js') > -1, 'MyComponent.js');
+                            t.end();
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+test('teardown', () => del(TESTDIR, { force: true }));
