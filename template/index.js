455f0c61535ed9e7b6f8a0acc2b3a24384e45411
Jesper Thøgersen <jesper@thog.no>
2016-01-13 19:22:35 +0100

    Revert "Updated template structure"
    This reverts commit d13666eaaa8c0fb74e241b1f82743ae895b15af0.

--- template/index.js
+++ template/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import MyComponent from 'components/MyComponent';
+import MyComponent from './MyComponent';
 
 ReactDOM.render(<MyComponent />, document.getElementById('root'));
