0f90ace6e4280a54a87a54768d29c6599ce5ce41
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-01-15 22:08:28 +0530

    Support requiring plain CSS

--- src/webpack-config.js
+++ src/webpack-config.js
@@ -37,6 +37,14 @@ module.exports = {
                 loader: 'json-loader'
             },
             {
+                test: /\.css$/,
+                loaders: [
+                    'style',
+                    'css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]',
+                    'autoprefixer?browsers=last 2 version'
+                ]
+            },
+            {
                 test: /\.less$/,
                 loaders: [
                     'style',
