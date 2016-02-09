27f98af78df5626c53e41fcea08faab5ae944c80
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-10 02:41:33 +0530

    Use postcss-loader

--- src/webpack-config.js
+++ src/webpack-config.js
@@ -2,6 +2,7 @@
 
 const webpack = require('webpack');
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const babelrc = require('./babelrc');
 
 const CURRENTDIR = path.join(__dirname, '..');
@@ -10,46 +11,50 @@ const BABEL_LOADER = 'babel-loader?' + JSON.stringify(babelrc);
 const STYLE_LOADERS = [
     'style-loader',
     'css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]',
-    'autoprefixer-loader?browsers=last 2 version'
+    'postcss-loader'
 ];
+const POSTCSS_OPTIONS = [ autoprefixer({ browsers: [ 'last 2 versions' ] }) ];
 
 module.exports = {
     devtool: 'inline-source-map',
     plugins: [
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
     ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: BABEL_LOADER
+                loader: BABEL_LOADER,
             },
             {
                 test: /\.(cjsx|coffee)$/,
-                loaders: [ BABEL_LOADER, 'coffee-loader', 'cjsx-loader' ]
+                loaders: [ BABEL_LOADER, 'coffee-loader', 'cjsx-loader' ],
             },
             {
                 test: /\.json$/,
-                loader: 'json-loader'
+                loader: 'json-loader',
             },
             {
                 test: /\.css$/,
-                loaders: STYLE_LOADERS
+                loaders: STYLE_LOADERS,
+                postcss: POSTCSS_OPTIONS,
             },
             {
                 test: /\.less$/,
                 loaders: [
                     ...STYLE_LOADERS,
                     'less-loader?outputStyle=expanded&sourceMap'
-                ]
+                ],
+                postcss: POSTCSS_OPTIONS,
             },
             {
                 test: /\.scss$/,
                 loaders: [
                     ...STYLE_LOADERS,
                     'sass-loader?outputStyle=expanded&sourceMap'
-                ]
+                ],
+                postcss: POSTCSS_OPTIONS,
             },
         ],
     },
