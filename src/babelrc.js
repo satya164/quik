53a9713da25bc0caa4915614f94b794abe5b47fe
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-23 17:18:06 +0530

    Add additional babel plugins to optimize bundle

--- src/babelrc.js
+++ src/babelrc.js
@@ -4,6 +4,17 @@ module.exports = {
     presets: [
         require.resolve('babel-preset-es2015-native-modules'),
         require.resolve('babel-preset-react'),
-        require.resolve('babel-preset-stage-1')
-    ]
+        require.resolve('babel-preset-stage-1'),
+    ],
+    plugins: [
+        require.resolve('babel-plugin-transform-runtime'),
+    ],
+    env: {
+        production: {
+            plugins: [
+                require.resolve('babel-plugin-transform-react-constant-elements'),
+                require.resolve('babel-plugin-transform-react-inline-elements'),
+            ]
+        }
+    }
 };
