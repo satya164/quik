79c9777d0075f150c273bce5eea353533280abaa
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-25 05:07:37 +0530

    Simplify babelrc

--- src/babelrc.js
+++ src/babelrc.js
@@ -1,17 +1,17 @@
 export default {
     presets: [
-        require.resolve('babel-preset-es2015-native-modules'),
-        require.resolve('babel-preset-react'),
-        require.resolve('babel-preset-stage-1'),
+        'es2015-native-modules',
+        'react',
+        'babel-preset-stage-1',
     ],
     plugins: [
-        require.resolve('babel-plugin-transform-runtime'),
+        'transform-runtime',
     ],
     env: {
         production: {
             plugins: [
-                require.resolve('babel-plugin-transform-react-constant-elements'),
-                require.resolve('babel-plugin-transform-react-inline-elements'),
+                'transform-react-constant-elements',
+                'transform-react-inline-elements',
             ]
         }
     }
