3ad5a9fb4a46044c45978d24d12d11a29503d2de
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-09-13 04:22:13 +0530

    fix(babel): don't use preset es2015-native-modules

--- src/babelrc.js
+++ src/babelrc.js
@@ -1,6 +1,6 @@
 export default {
     presets: [
-        require.resolve('babel-preset-es2015-native-modules'),
+        [ require.resolve('babel-preset-es2015'), { 'modules': false } ],
         require.resolve('babel-preset-react'),
         require.resolve('babel-preset-stage-1'),
     ],
